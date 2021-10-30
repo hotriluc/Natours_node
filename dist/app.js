80317ace39b2bd2b421fd6b315aaf769ff7be27d
Luc Ho <hotriluc97@gmail.com>
2021-10-30 22:27:02 +0300

    post endpoint

--- dist/app.js
+++ dist/app.js
@@ -6,10 +6,20 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const fs_1 = __importDefault(require("fs"));
 const app = (0, express_1.default)();
+app.use(express_1.default.json());
 const port = 3000;
-const tours = JSON.parse(fs_1.default.readFileSync('dev-data/data/tours.json').toString());
-app.get('/api/v1/tours', (req, res) => {
+const tours = JSON.parse(fs_1.default.readFileSync('dev-data/data/tours-simple.json').toString());
+app
+    .get('/api/v1/tours', (req, res) => {
     res.status(200).json({ message: 'success', data: { tours: tours } });
+})
+    .post('/api/v1/tours', (req, res) => {
+    let tourId = tours[tours.length - 1].id + 1;
+    let tour = Object.assign({ id: tourId }, req.body);
+    tours.push(tour);
+    fs_1.default.writeFile('dev-data/data/tours-simple.json', JSON.stringify(tours), (err) => {
+        res.send('ok');
+    });
 });
 app.listen(port, () => {
     console.log('This server is running on port ' + port);
--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,22 @@ import express, { Request, Response } from 'express';
 import fs from 'fs';
 
 const app = express();
+app.use(express.json());
 const port = 3000;
-const tours = JSON.parse(fs.readFileSync('dev-data/data/tours.json').toString());
+const tours = JSON.parse(fs.readFileSync('dev-data/data/tours-simple.json').toString());
 
-app.get('/api/v1/tours', (req: Request, res: Response) => {
-  res.status(200).json({ message: 'success', data: { tours: tours } });
-});
+app
+  .get('/api/v1/tours', (req: Request, res: Response) => {
+    res.status(200).json({ message: 'success', data: { tours: tours } });
+  })
+  .post('/api/v1/tours', (req: Request, res: Response) => {
+    let tourId = tours[tours.length - 1].id + 1;
+    let tour = Object.assign({ id: tourId }, req.body);
+    tours.push(tour);
+    fs.writeFile('dev-data/data/tours-simple.json', JSON.stringify(tours), (err) => {
+      res.send('ok');
+    });
+  });
 
 app.listen(port, () => {
   console.log('This server is running on port ' + port);
