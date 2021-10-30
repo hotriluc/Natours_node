0c3978f7e2d5e9c885125c5f26b7ca0e2dccc351
Luc Ho <hotriluc97@gmail.com>
2021-10-30 22:54:45 +0300

    CRUD for tours

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import e from 'express';
 import express, { Request, Response } from 'express';
 import fs from 'fs';
 
@@ -9,6 +10,42 @@ const tours = JSON.parse(fs.readFileSync('dev-data/data/tours-simple.json').toSt
 app
   .get('/api/v1/tours', (req: Request, res: Response) => {
     res.status(200).json({ message: 'success', data: { tours: tours } });
+  })
+  .get('/api/v1/tours/:id', (req: Request, res) => {
+    const id = parseInt(req.params['id']);
+    const tour = tours.find((el) => el.id === id);
+
+    if (!tour) {
+      res.status(404).json({ status: 'fail', message: 'Invalid ID' });
+    } else {
+      res.status(200).json({ status: 'success', data: { tour: tour } });
+    }
+  })
+  .patch('/api/v1/tours/:id', (req: Request, res: Response) => {
+    const id = parseInt(req.params['id']);
+    const tour = tours.find((el) => el.id === id);
+
+    if (!tour) {
+      res.status(404).json({ status: ' fail', message: 'Invalid ID' });
+    } else {
+      tour['price'] = 'TOBEUPDATED';
+      fs.writeFile('dev-data/data/tours-simple.json', JSON.stringify(tours), (err) => {
+        res.status(200).json({ status: 'success', tour: tour });
+      });
+    }
+  })
+  .delete('/api/v1/tours/:id', (req: Request, res: Response) => {
+    const id = parseInt(req.params['id']);
+    const tour = tours.find((el) => el.id === id);
+
+    if (!tour) {
+      res.status(404).json({ status: ' fail', message: 'Invalid ID' });
+    } else {
+      tours.splice(id, id);
+      fs.writeFile('dev-data/data/tours-simple.json', JSON.stringify(tours), (err) => {
+        res.status(200).json({ status: 'success', tour: tour });
+      });
+    }
   })
   .post('/api/v1/tours', (req: Request, res: Response) => {
     let tourId = tours[tours.length - 1].id + 1;
